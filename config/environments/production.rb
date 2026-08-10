6ab9a17eae483f24a05ecd055818ee975b01813c
Sae Nakatani <s.n.bsk0218@gmail.com>
2026-08-09 19:15:13 -0700

    fix: set config .hosts explicitly for render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   # Render のドメイン（.onrender.com）からのアクセスを許可
-  config.hosts << ".onrender.com"
-  config.hosts << "final-project-d423.onrender.com"
+  config.hosts = [
+    ".onrender.com",
+    "final-project-d423.onrender.com"
+  ]
 end
