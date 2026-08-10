7881e8aaf36769610c8870736a82be3feee43ddc
Sae Nakatani <s.n.bsk0218@gmail.com>
2026-08-09 21:46:09 -0700

    fix: set config.hosts in application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.hosts << ".onrender.com"
+    config.hosts << "final-project-d423.onrender.com"
+    
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
