bf6ed141221ecccf755a8d20325d7a0101303b83
Sae Nakatani <s.n.bsk0218@gmail.com>
2026-08-11 16:03:07 -0700

    Add users and user_sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  
   get 'static_pages/top'
   root 'static_pages#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  #User registration
+  resources :user, only: %i[new create]
+
+  #Login / Logout
+  get 'login' => 'user_sessions#new', as: :login
+  post 'login' => 'user_sessions#create'
+  delete 'logout' => 'user_sessions#destroy', as: :logout
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
